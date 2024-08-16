12e6257c2d15db0b61d0998f44ce3bbd4458a347
Jean Pierry de Souza Mendonça <pierrybm@gmail.com>
2024-08-16 10:38:56 -0300

    UNDO TEST CHANGES

--- app/javascript/App/Pages/Select.jsx
+++ app/javascript/App/Pages/Select.jsx
@@ -21,18 +21,9 @@ export default function SelectDemo() {
           <SelectLabel>Fruits</SelectLabel>
           <SelectItem value="apple">Apple</SelectItem>
           <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="orange">Orange</SelectItem>
-          <SelectItem value="watermelon">Watermelon</SelectItem>
-          <SelectItem value="watermelon_0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer</SelectItem>
-          <SelectItem value="watermelon_1">LoremIpsumhasbeentheindustrysstandarddummytexteversincethe1500swhenanunknownprinter</SelectItem>
-          <SelectItem value="watermelon_2">Watermelon_2</SelectItem>
-          <SelectItem value="watermelon_3">Watermelon_3</SelectItem>
-          <SelectItem value="watermelon_4">Watermelon_4</SelectItem>
-          <SelectItem value="watermelon_5">Watermelon_5</SelectItem>
-          <SelectItem value="watermelon_6">Watermelon_6</SelectItem>
-          <SelectItem value="watermelon_7">Watermelon_7</SelectItem>
-          <SelectItem value="watermelon_8">Watermelon_8</SelectItem>
-          <SelectItem value="watermelon_9">Watermelon_9</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
--- app/views/phlex/select.rb
+++ app/views/phlex/select.rb
@@ -17,16 +17,6 @@ module Phlex
                 SelectItem(value: "orange") { "Orange" }
                 SelectItem(value: "banana") { "Banana" }
                 SelectItem(value: "watermelon") { "Watermelon" }
-                SelectItem(value: "watermelon_0") { "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer" }
-                SelectItem(value: "watermelon_1") { "LoremIpsumhasbeentheindustrysstandarddummytexteversincethe1500swhenanunknownprinter" }
-                SelectItem(value: "watermelon_2") { "Watermelon_2" }
-                SelectItem(value: "watermelon_3") { "Watermelon_3" }
-                SelectItem(value: "watermelon_4") { "Watermelon_4" }
-                SelectItem(value: "watermelon_5") { "Watermelon_5" }
-                SelectItem(value: "watermelon_6") { "Watermelon_6" }
-                SelectItem(value: "watermelon_7") { "Watermelon_7" }
-                SelectItem(value: "watermelon_8") { "Watermelon_8" }
-                SelectItem(value: "watermelon_9") { "Watermelon_9" }
               end
             end
           end
