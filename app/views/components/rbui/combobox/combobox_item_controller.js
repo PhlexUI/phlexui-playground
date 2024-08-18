f74c22ca26acab18c2d57c02d449fcb7a99839b5
Cirdes <cirdes@gmail.com>
2024-08-18 18:53:31 +0000

    Working version

--- app/views/components/rbui/combobox/combobox_item_controller.js
+++ app/views/components/rbui/combobox/combobox_item_controller.js
@@ -1,88 +1,96 @@
 import { Controller } from "@hotwired/stimulus";
-import { ITEM_KEY_UP, ITEM_KEY_DOWN, ITEM_KEY_ENTER } from "./combobox_content_controller";
+// import { ITEM_KEY_UP, ITEM_KEY_DOWN, ITEM_KEY_ENTER } from "./combobox_content_controller";
 
-export const ITEM_SELECTED = "rbui--combobox-item#selected";
-const ITEM_MOUSEENTER = "rbui--combobox-item#mouseenter";
+// export const ITEM_SELECTED = "rbui--combobox-item#selected";
+// const ITEM_MOUSEENTER = "rbui--combobox-item#mouseenter";
 
 export default class extends Controller {
-  static targets = ["check"];
+  // static targets = ["check"];
 
   connect() {
-    document.addEventListener(ITEM_SELECTED, (e) => this.uncheck(e.detail), false);
-    document.addEventListener(ITEM_MOUSEENTER, (e) => this.unselect(e.detail), false);
-    document.addEventListener(ITEM_KEY_UP, (e) => this.handleKeyUp(e.detail), false);
-    document.addEventListener(ITEM_KEY_DOWN, (e) => this.handleKeyDown(e.detail), false);
-    document.addEventListener(ITEM_KEY_ENTER, (e) => this.handleKeyEnter(e.detail), false);
+    // document.addEventListener(ITEM_SELECTED, (e) => this.uncheck(e.detail), false);
+    // document.addEventListener(ITEM_MOUSEENTER, (e) => this.unselect(e.detail), false);
+    // document.addEventListener(ITEM_KEY_UP, (e) => this.handleKeyUp(e.detail), false);
+    // document.addEventListener(ITEM_KEY_DOWN, (e) => this.handleKeyDown(e.detail), false);
+    // document.addEventListener(ITEM_KEY_ENTER, (e) => this.handleKeyEnter(e.detail), false);
   }
 
   disconnect() {
-    document.removeEventListener(ITEM_SELECTED, (e) => this.uncheck(e.detail), false);
-    document.removeEventListener(ITEM_MOUSEENTER, (e) => this.unselect(e.detail), false);
-    document.removeEventListener(ITEM_KEY_UP, (e) => this.handleKeyUp(e.detail), false);
-    document.removeEventListener(ITEM_KEY_DOWN, (e) => this.handleKeyDown(e.detail), false);
-    document.removeEventListener(ITEM_KEY_ENTER, (e) => this.handleKeyEnter(e.detail), false);
+    // document.removeEventListener(ITEM_SELECTED, (e) => this.uncheck(e.detail), false);
+    // document.removeEventListener(ITEM_MOUSEENTER, (e) => this.unselect(e.detail), false);
+    // document.removeEventListener(ITEM_KEY_UP, (e) => this.handleKeyUp(e.detail), false);
+    // document.removeEventListener(ITEM_KEY_DOWN, (e) => this.handleKeyDown(e.detail), false);
+    // document.removeEventListener(ITEM_KEY_ENTER, (e) => this.handleKeyEnter(e.detail), false);
   }
 
-  mouseenter() {
-    this.element.setAttribute("aria-selected", true);
-
-    const { value } = this.element.dataset;
-    const event = new CustomEvent(ITEM_MOUSEENTER, { detail: { value } });
-    document.dispatchEvent(event);
-  }
-
-  selectItem() {
-    this.checkTarget.classList.toggle("invisible", false);
-
-    const { value } = this.element.dataset;
-    const label = this.element.innerText;
-
-    const event = new CustomEvent(ITEM_SELECTED, { detail: { value, label } });
-    document.dispatchEvent(event);
-  }
-
-  handleKeyUp({ id }) {
-    const [base, idx] = id.split("-");
-
-    if (idx === "0") return;
-
-    const prevIdx = parseInt(idx) - 1;
-
-    if (this.element.id === `${base}-${prevIdx}`) {
+  handleItemSelected(value) {
+    if (this.element.dataset.value == value) {
       this.element.setAttribute("aria-selected", true);
     } else {
       this.element.removeAttribute("aria-selected");
     }
   }
 
-  handleKeyDown({ id, length }) {
-    const [base, idx] = id.split("-");
-    const nextIdx = parseInt(idx) + 1;
+  // mouseenter() {
+  //   this.element.setAttribute("aria-selected", true);
 
-    if (nextIdx === length) return;
+  //   const { value } = this.element.dataset;
+  //   const event = new CustomEvent(ITEM_MOUSEENTER, { detail: { value } });
+  //   document.dispatchEvent(event);
+  // }
 
-    if (this.element.id === `${base}-${nextIdx}`) {
-      this.element.setAttribute("aria-selected", true);
-    } else {
-      this.element.removeAttribute("aria-selected");
-    }
-  }
+  // selectItem() {
+  //   this.checkTarget.classList.toggle("invisible", false);
 
-  handleKeyEnter({ id }) {
-    if (this.element.id !== id) return;
+  //   const { value } = this.element.dataset;
+  //   const label = this.element.innerText;
 
-    this.selectItem();
-  }
+  //   // const event = new CustomEvent(ITEM_SELECTED, { detail: { value, label } });
+  //   document.dispatchEvent(event);
+  // }
 
-  uncheck({ value }) {
-    if (this.element.dataset.value !== value) {
-      this.checkTarget.classList.toggle("invisible", true);
-    }
-  }
+  // handleKeyUp({ id }) {
+  //   const [base, idx] = id.split("-");
 
-  unselect({ value }) {
-    if (this.element.dataset.value !== value) {
-      this.element.removeAttribute("aria-selected");
-    }
-  }
+  //   if (idx === "0") return;
+
+  //   const prevIdx = parseInt(idx) - 1;
+
+  //   if (this.element.id === `${base}-${prevIdx}`) {
+  //     this.element.setAttribute("aria-selected", true);
+  //   } else {
+  //     this.element.removeAttribute("aria-selected");
+  //   }
+  // }
+
+  // handleKeyDown({ id, length }) {
+  //   const [base, idx] = id.split("-");
+  //   const nextIdx = parseInt(idx) + 1;
+
+  //   if (nextIdx === length) return;
+
+  //   if (this.element.id === `${base}-${nextIdx}`) {
+  //     this.element.setAttribute("aria-selected", true);
+  //   } else {
+  //     this.element.removeAttribute("aria-selected");
+  //   }
+  // }
+
+  // handleKeyEnter({ id }) {
+  //   if (this.element.id !== id) return;
+
+  //   this.selectItem();
+  // }
+
+  // uncheck({ value }) {
+  //   if (this.element.dataset.value !== value) {
+  //     this.checkTarget.classList.toggle("invisible", true);
+  //   }
+  // }
+
+  // unselect({ value }) {
+  //   if (this.element.dataset.value !== value) {
+  //     this.element.removeAttribute("aria-selected");
+  //   }
+  // }
 }
